2ed6d863bc361104e56b9dda1f13712e014e2f66
Mehdi Moshiri asl <moshiriemail@gmail.com>
2024-11-16 22:56:10 +0100

    receiver ok

---
+++ MapReceivers/MapReceivers/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace MapReceivers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_display = new System.Windows.Forms.TextBox();
+            this.btn_pull = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_display
+            // 
+            this.txt_display.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_display.Location = new System.Drawing.Point(23, 39);
+            this.txt_display.Name = "txt_display";
+            this.txt_display.Size = new System.Drawing.Size(232, 40);
+            this.txt_display.TabIndex = 3;
+            // 
+            // btn_pull
+            // 
+            this.btn_pull.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pull.Location = new System.Drawing.Point(291, 27);
+            this.btn_pull.Name = "btn_pull";
+            this.btn_pull.Size = new System.Drawing.Size(259, 67);
+            this.btn_pull.TabIndex = 2;
+            this.btn_pull.Text = "Pull";
+            this.btn_pull.UseVisualStyleBackColor = true;
+            this.btn_pull.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 114);
+            this.Controls.Add(this.txt_display);
+            this.Controls.Add(this.btn_pull);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mapped File  Receiver Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_display;
+        private System.Windows.Forms.Button btn_pull;
+    }
+}
+
---
+++ MapReceivers/MapReceivers/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MapReceivers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Befor run this App (this code) you must run the first app (Sender) to create this variable in memory
+            using (var mmf = MemoryMappedFile.OpenExisting("MMport1"))  
+            {
+                using (var accessor = mmf.CreateViewAccessor(0, 20))
+                {
+
+                    double A = accessor.ReadDouble(0);
+                    txt_display.Text = Convert.ToString(A);
+
+                }
+            }
+        }
+    }
+}
