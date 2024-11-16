e16046b7b7b7b446ede9c83468102f429df706f1
Mehdi Moshiri asl <moshiriemail@gmail.com>
2024-11-16 23:55:09 +0100

    Add Sender

---
+++ MapSender/MapSender/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace MapSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_push = new System.Windows.Forms.Button();
+            this.txt_display = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_push
+            // 
+            this.btn_push.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_push.Location = new System.Drawing.Point(298, 25);
+            this.btn_push.Name = "btn_push";
+            this.btn_push.Size = new System.Drawing.Size(259, 67);
+            this.btn_push.TabIndex = 5;
+            this.btn_push.Text = "Push";
+            this.btn_push.UseVisualStyleBackColor = true;
+            this.btn_push.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_display
+            // 
+            this.txt_display.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_display.Location = new System.Drawing.Point(19, 37);
+            this.txt_display.Name = "txt_display";
+            this.txt_display.Size = new System.Drawing.Size(232, 40);
+            this.txt_display.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 116);
+            this.Controls.Add(this.btn_push);
+            this.Controls.Add(this.txt_display);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mapped File Sender ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_push;
+        private System.Windows.Forms.TextBox txt_display;
+    }
+}
+
---
+++ MapSender/MapSender/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MapSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateMapFileOnRam();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        MemoryMappedFile mmf;
+        private bool CreateMapFileOnRam()
+        {
+            bool shart = false;
+            try
+            {
+                // CreateNew Table on memory
+                mmf = MemoryMappedFile.CreateNew("MMport1", 20);
+                if (mmf != null) shart = true;
+            }
+            catch (Exception ex)
+            {
+                shart = false;
+            }
+            return shart;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = 0;
+            if (double.TryParse(txt_display.Text.Trim(), out a))
+            {
+                SendToRam(a);
+            }
+            else
+            {
+                MessageBox.Show("Enter Number ,Please");
+            }
+        }
+
+        private bool SendToRam(double Number)
+        {
+            bool shart = false;
+            try
+            {
+                // push number to Memory
+                MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor();
+                accessor.Write(0, Number);
+                accessor.Dispose();
+
+                shart = true;
+            }
+            catch (Exception ex)
+            {
+                shart = false;
+            }
+            return shart;
+        }
+    }
+}
